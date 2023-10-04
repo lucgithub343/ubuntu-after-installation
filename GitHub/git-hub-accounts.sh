23a5e1fe2665bb50324a30988213abcfe846dd5c
lucgithub343 <luc.github@hotmail.com>
2023-10-03 22:27:08 -0300

    changing the file

--- GitHub/git-hub-accounts.sh
+++ GitHub/git-hub-accounts.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+## Function to create account lucotavio
+creating_account-lucotavio(){
 
-## Entrando na para pasta de programas
-cd /home/$USER/Downloads/Softwares/
+  echo "#lucotavio account" | tee  ~/.ssh/config
+  echo "Host github.com-lucotavio" | tee -a ~/.ssh/config
+  echo "     HostName github.com" | tee -a ~/.ssh/config
+  echo "     User git" | tee -a ~/.ssh/config
+  echo "     IdentityFile ~/.ssh/lucotavio" | tee -a ~/.ssh/config
+
+  ## Creating empty space between accounts
+  echo "" | tee -a ~/.ssh/config
+
+}
+
+
+
+## Function to create account lucgithub343
+creating_account_lucgithub343(){
+
+  echo "#lucgithub343 account" | tee -a ~/.ssh/config
+  echo "Host github.com-lucgithub343" | tee -a ~/.ssh/config
+  echo "     HostName github.com" | tee -a ~/.ssh/config
+  echo "     User git" | tee -a ~/.ssh/config
+  echo "     IdentityFile ~/.ssh/lucgithub343" | tee -a ~/.ssh/config
+
+}
 
 
 
@@ -22,22 +45,35 @@ if [ -e ~/.ssh/config ];
 then
     echo "O arquivo  ~/.ssh/config  ja existe"
 else
-    echo "Copiando o arquivo config para pasta ~/.ssh/"
-    git clone https://github.com/lucgithub343/git-hub-accounts.git
-    cd git-hub-accounts/
-    cp config ~/.ssh/
+    echo "Criando o arquivo config na pasta ~/.ssh/"
+
+    ## Calling function
+    creating_account-lucotavio
+
+    ## Calling function
+    creating_account_lucgithub343
 fi
 
 
 
 ## Configurando conta lucotavio
-ssh-add -K ~/.ssh/lucotavio
-gedit ~/.ssh/lucotavio.pub
-ssh-add -k ~/.ssh/lucotavio
+if [ -e ~/.ssh/lucotavio ];
+then
+    echo "O arquivo  ~/.ssh/lucotavio  ja existe"
+else
+    ssh-add -K ~/.ssh/lucotavio
+    gedit ~/.ssh/lucotavio.pub
+    ssh-add -k ~/.ssh/lucotavio
+fi
 
 
 
 ## Configurando conta lucgithub343
-ssh-add -K ~/.ssh/lucgithub343
-gedit ~/.ssh/lucgithub343.pub
-ssh-add -k ~/.ssh/lucgithub343
+if [ -e ~/.ssh/lucgithub343 ];
+then
+    echo "O arquivo  ~/.ssh/lucgithub343  ja existe"
+else
+    ssh-add -K ~/.ssh/lucgithub343
+    gedit ~/.ssh/lucgithub343.pub
+    ssh-add -k ~/.ssh/lucgithub343
+fi
