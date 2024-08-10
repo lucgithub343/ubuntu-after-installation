4d10d304a3bc7986de4440db0208f3d47b8075ce
lucgithub343 <luc.github@hotmail.com>
2024-08-10 01:19:00 -0300

    removendo coisas antigas e colocando coisas novas

--- reinstalling-icon-deepin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-## Entrando na para pasta de programas
-cd /home/$USER/Downloads/Softwares/
-
-rm -r /home/$USER/.icons/Uos-fulldistro-icons/
-cp -r Uos-fulldistro-icons/ /home/$USER/.icons/
--- removing-node.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-sudo apt purge nodejs -y
-sudo rm -r /etc/apt/sources.list.d/nodesource.list
-sudo rm -r /etc/apt/keyrings/nodesource.gpg
